b7f3eab83b4843f192bf5a394b940cfda1a24bdc
Francisco Pfeiffer <pfeifferfran1@gmail.com>
2023-09-06 15:05:05 -0300

    Estructure la pagina y rellene con alginos datos

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,22 +1,45 @@
 export const Header = () => {
     return (
         <>
-            <div className="header-container">
-
-                <div className="header-texto">
-                    <h1 className="h1-header">
-                    Estudio Juridico I-F & ASOSCIADOS
-                    </h1>
+            <div className="estudioif">
+                <header className='header-container'>
+                    <h1 className="h1-header">Estudio Juridico I-F <br /> & ASOSCIADOS</h1>
                     <h4 className="h4-header">
-                    Atencion las <strong>24hs</strong> los 365 dias del año
+                        Atención las <strong>24hs</strong> los 365 dias del año
                     </h4>
-                    <div className="button-header">
-                        <a href="#">Contacto</a>
-                    </div>
-                    <div className="button-header">
-                        <a href="#">Servicio</a>
-                    </div>
-                </div>
+                </header>
+                <main>
+                    <section>
+                        <h2>Sobre Nosotros</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Suspendisse potenti. Maecenas scelerisque, lectus ac
+                            tincidunt semper, eros quam ultricies eros, eu scelerisque
+                            nibh velit ac quam.
+                        </p>
+                    </section>
+                    <section>
+                        <h2>Servicios</h2>
+                        <ul>
+                            <li>D Penal</li>
+                            <li>D Familia y Sucesiones</li>
+                            <li>D Privado</li>
+                            <li>D Daños Personales</li>
+                            <li>D Laboral</li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h2>Contact Us</h2>
+                        <p>
+                            Ubicacion: Francia 3055 - Planta Baja - Oficina 1 <br />
+                            Tel: 0342-4122103 - 156127501 (urgencias) <br />
+                            E-mail: est.jco.nt@hotmail.com
+                        </p>
+                    </section>
+                </main>
+                <footer>
+                    <p>Copyright © 2023 Estudio I - F & ASOCIADOS</p>
+                </footer>
             </div>
         </>
     )
